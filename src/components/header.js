119ffb55b816c7935ca3cbd6f6ca65e79ad90c79
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-08-09 03:00:34 -0500

    adds styles with css modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
+import HeaderStyles from "./header.module.scss"
 
 function Header() {
   return (
     <>
-      <header>
-        <h1>Frank Pigeon</h1>
-        <ul>
-          <Link to="/">
+      <header className={HeaderStyles.header}>
+        <h1>
+          <Link className={HeaderStyles.title} to="/">
+            Frank Pigeon
+          </Link>
+        </h1>
+        <ul className={HeaderStyles.navList}>
+          <Link
+            className={HeaderStyles.navItem}
+            activeClassName={HeaderStyles.activeNavItem}
+            to="/"
+          >
             <li>Home</li>
           </Link>
-          <Link to="/about">
+          <Link
+            className={HeaderStyles.navItem}
+            activeClassName={HeaderStyles.activeNavItem}
+            to="/about"
+          >
             <li>About</li>
           </Link>
-          <Link to="/blog">
+          <Link
+            className={HeaderStyles.navItem}
+            activeClassName={HeaderStyles.activeNavItem}
+            to="/blog"
+          >
             <li>Blog</li>
           </Link>
-          <Link to="/contact">
+          <Link
+            className={HeaderStyles.navItem}
+            activeClassName={HeaderStyles.activeNavItem}
+            to="/contact"
+          >
             <li>Contact</li>
           </Link>
         </ul>
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,17 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
+import LayoutStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <Header />
-      {children}
+    <div className={LayoutStyles.container}>
+      <main className={LayoutStyles.content}>
+        <Header />
+        {children}
+      </main>
       <Footer />
-    </>
+    </div>
   )
 }
 
