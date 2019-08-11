26243b863a5591031af4c0abeb9bf5bc0a38462e
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-08-11 01:09:34 -0500

    starts blog posts and adds graphql. GraphQL will  get all blog posts to the blog page.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,34 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  const { allMarkdownRemark } = data // data.markdownRemark holds our post data
   return (
     <Layout>
       <h1>My Blog </h1>
-      <p>Posts will show up later.</p>
+      <ol>
+        {allMarkdownRemark.edges.map(post => (
+          <li key={post.node.frontmatter.title}>
+            <h2>{post.node.frontmatter.title}</h2>
+            <p>{post.node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }
